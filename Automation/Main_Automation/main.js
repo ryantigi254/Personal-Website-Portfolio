c4c9042fef818ec5e69e05397af46c0c08340d9f
Ryan Gichuru <ryantigi2020@gmail.com>
2024-03-12 04:17:50 +0000

    Delete unused assets and update dropdown menu functionality(Javascript)

--- Automation/Main_Automation/main.js
+++ Automation/Main_Automation/main.js
@@ -1,15 +1,18 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const moreList = document.querySelector('.more-dropdown-list');
+document.addEventListener('DOMContentLoaded', () => {
+    // Define the elements outside the event listener scope
+    const moreToggle = document.querySelector('.dropdown-toggle');
     const dropdownContent = document.querySelector('.dropdown-content');
 
     // Toggle the dropdown on click
-    moreList.addEventListener('click', function() {
+    moreToggle.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default anchor link behavior
+        event.stopPropagation(); // Prevent click from being immediately propagated to the window
         dropdownContent.classList.toggle('active');
     });
 
     // Close the dropdown if the user clicks outside of it
     window.addEventListener('click', function(e) {
-        if (!moreList.contains(e.target) && !dropdownContent.contains(e.target)) {
+        if (!moreToggle.contains(e.target) && !dropdownContent.contains(e.target)) {
             dropdownContent.classList.remove('active');
         }
     });
