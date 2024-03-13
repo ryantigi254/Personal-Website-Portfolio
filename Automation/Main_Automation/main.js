b889605a7fb13a4a6c3f0b4b62cd11673791393e
Ryan Gichuru <ryantigi2020@gmail.com>
2024-03-13 06:32:49 +0000

    Update About Page, remove unused files, and add more assets

--- Automation/Main_Automation/main.js
+++ Automation/Main_Automation/main.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Define the elements outside the event listener scope
     const moreToggle = document.querySelector('.dropdown-toggle');
     const dropdownContent = document.querySelector('.dropdown-content');
 
-    // Toggle the dropdown on click
     moreToggle.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent the default anchor link behavior
-        event.stopPropagation(); // Prevent click from being immediately propagated to the window
+        event.preventDefault();
+        event.stopPropagation();
         dropdownContent.classList.toggle('active');
     });
 
-    // Close the dropdown if the user clicks outside of it
     window.addEventListener('click', function(e) {
         if (!moreToggle.contains(e.target) && !dropdownContent.contains(e.target)) {
             dropdownContent.classList.remove('active');
