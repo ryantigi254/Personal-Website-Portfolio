c847e4af540d65ddf2ec320fd59618ff92f63ee5
Ryan Gichuru <ryantigi2020@gmail.com>
2024-03-13 06:43:59 +0000

    Reverted to ensure a branch iscreated for the page

--- Automation/Main_Automation/main.js
+++ Automation/Main_Automation/main.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Define the elements outside the event listener scope
     const moreToggle = document.querySelector('.dropdown-toggle');
     const dropdownContent = document.querySelector('.dropdown-content');
 
+    // Toggle the dropdown on click
     moreToggle.addEventListener('click', function(event) {
-        event.preventDefault();
-        event.stopPropagation();
+        event.preventDefault(); // Prevent the default anchor link behavior
+        event.stopPropagation(); // Prevent click from being immediately propagated to the window
         dropdownContent.classList.toggle('active');
     });
 
+    // Close the dropdown if the user clicks outside of it
     window.addEventListener('click', function(e) {
         if (!moreToggle.contains(e.target) && !dropdownContent.contains(e.target)) {
             dropdownContent.classList.remove('active');
